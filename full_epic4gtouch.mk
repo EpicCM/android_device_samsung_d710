87117afa29996654c21c26f0068ae9fe662ae331
Ameer Ghouse <aghouse1@gmail.com>
2012-03-24 16:50:00 -0500

    full_epic4gtouch.mk: inherit proper makefile

--- full_epic4gtouch.mk
+++ full_epic4gtouch.mk
@@ -22,8 +22,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/epic4gtouch/epic4gtouch.mk)
 
 # Discard inherited values and use our own instead.
