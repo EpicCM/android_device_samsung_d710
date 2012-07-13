b53ecd886b95f19382c2335559ecc45a3e1959aa
Chris41g <chriswhite@cableone.net>
2012-07-13 10:03:55 -0500

    inline kernel for cyanogenmod

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,7 +51,10 @@ BOARD_NAND_SPARE_SIZE := 128
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_BASE := 0x40000000
 BOARD_KERNEL_CMDLINE := console=ttySAC2,115200 consoleblank=0
-TARGET_PREBUILT_KERNEL := device/samsung/epic4gtouch/kernel
+
+# Inline kernel building
+TARGET_KERNEL_SOURCE := kernel/samsung/epic4gtouch
+TARGET_KERNEL_CONFIG := cyanogenmod_epic4gtouch_defconfig
 
 # Filesystem
 TARGET_USERIMAGES_USE_EXT4 := true
