8b3e380a28791e1524c94a1779cd9a2d8b270c27
chris41g <chriswhite41g@gmail.com>
2012-08-14 11:08:12 -0500

    storage

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,12 +145,12 @@ BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := false
 BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
 
 # MTP
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/f_mass_storage/lun%d/file"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/s3c-usbgadget/gadget/lun%d/file"
 
 # Recovery
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/epic4gtouch/recovery/recovery_keys.c
 BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/epic4gtouch/recovery/graphics.c
-BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun0/file"
+BOARD_UMS_LUNFILE := "/sys/devices/platform/s3c-usbgadget/gadget/lun%d/file"
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_MISC_PARTITION := true
