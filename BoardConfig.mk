0df8dde372828fd02e2d32474da849c2b656fd3d
Chris41g <chriswhite41g@gmail.com>
2012-08-14 23:06:18 -0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -152,10 +152,11 @@ BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/epic4gtouch/recovery/re
 BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/epic4gtouch/recovery/graphics.c
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_USES_MMCUTILS := true
+BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun0/file"
 BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk0p11
-BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/cyasblkdevblk0p1
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/cyasblkdevblk0p1
+BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk0p11
 BOARD_SUPPRESS_EMMC_WIPE := true
 
 # Device specific headers
