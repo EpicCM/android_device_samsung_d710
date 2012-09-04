79aeb0da2cb20a1e00d96ee0dfc861a02aada5be
chris41g <chriswhite41g@gmail.com>
2012-09-04 12:52:25 -0500

    Fix rotation and gps
    fix rotation and gps by rearranging the vendor repos

--- d710.mk
+++ d710.mk
@@ -153,7 +153,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	lights.exynos4 \
 	sensors.exynos4 \
-	gps.exynos4 \
 	libhwconverter \
 	libs5pjpeg \
 	libfimg
@@ -250,5 +249,5 @@ TARGET_OMX_PATH := hardware/samsung/exynos/multimedia/openmax
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4330/device-bcm.mk)
 $(call inherit-product, hardware/samsung/exynos4210.mk)
 $(call inherit-product, frameworks/base/build/phone-hdpi-512-dalvik-heap.mk)
-$(call inherit-product-if-exists, vendor/samsung/d710/d710-vendor.mk)
 $(call inherit-product-if-exists, vendor/samsung/galaxys2-common/common-vendor-blobs.mk)
+$(call inherit-product-if-exists, vendor/samsung/d710/d710-vendor.mk)
