91fb28e6d527886915b1ca5c08a284264fd820cb
Chris41g <chriswhite41g@gmail.com>
2012-09-26 12:37:41 -0500

    Update DeviceSettings/src/com/cyanogenmod/settings/device/Utils.java

--- DeviceSettings/src/com/cyanogenmod/settings/device/Utils.java
+++ DeviceSettings/src/com/cyanogenmod/settings/device/Utils.java
@@ -24,6 +24,7 @@ import java.io.OutputStreamWriter;
 import java.io.FileWriter;
 import java.io.Reader;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
