b179d4784d65dcb84496c31acbfb8ce17cf6b25b
Chris41g <chriswhite41g@gmail.com>
2012-10-09 12:50:52 -0500

    Clean up, since using gs2-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,159 +17,40 @@
 
 # This variable is set first, so it can be overridden
 # by BoardConfigVendor.mk
-USE_CAMERA_STUB := true
-BOARD_USES_GENERIC_AUDIO := false
-BOARD_USES_LIBSECRIL_STUB := true
+-include device/samsung/galaxys2-common/BoardCommonConfig.mk
+
 BOARD_USE_SKIA_LCDTEXT := true
 
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_SMP := true
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_ARCH_VARIANT_CPU := cortex-a9
-ARCH_ARM_HAVE_TLS_REGISTER := true
-ARCH_ARM_HAVE_NEON := true
 TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
 
-EXYNOS4210_ENHANCEMENTS := true
-
-ifdef EXYNOS4210_ENHANCEMENTS
-COMMON_GLOBAL_CFLAGS += -DEXYNOS4_ENHANCEMENTS
-COMMON_GLOBAL_CFLAGS += -DEXYNOS4210_ENHANCEMENTS
-COMMON_GLOBAL_CFLAGS += -DSURFACEFLINGER_FORCE_SCREEN_RELEASE
-endif
-
-TARGET_BOARD_PLATFORM := exynos4
-TARGET_SOC := exynos4210
-TARGET_BOOTLOADER_BOARD_NAME := smdk4210
 TARGET_BOARD_INFO_FILE := device/samsung/d710/board-info.txt
-
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_RADIOIMAGE := true
-
-TARGET_PROVIDES_INIT := true
-TARGET_PROVIDES_INIT_TARGET_RC := true
 TARGET_RECOVERY_INITRC := device/samsung/d710/recovery.rc
-
-BOARD_NAND_PAGE_SIZE := 4096
-BOARD_NAND_SPARE_SIZE := 128
-BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_BASE := 0x40000000
-BOARD_KERNEL_CMDLINE := console=ttySAC2,115200 consoleblank=0
 
 # Kernel Config
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4210
 TARGET_KERNEL_CONFIG := cyanogenmod_d710_defconfig
 
-# Filesystem
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 536870912
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
-BOARD_FLASH_BLOCK_SIZE := 4096
-
-# Releasetools
-TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/galaxys2-common/releasetools/galaxys2_ota_from_target_files
-TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/galaxys2-common/releasetools/galaxys2_img_from_target_files
-
 # Graphics
-BOARD_EGL_CFG := device/samsung/galaxys2-common/configs/egl.cfg
-USE_OPENGL_RENDERER := true
 EGL_ALWAYS_ASYNC := true
-
-# Enable WEBGL in WebKit
-ENABLE_WEBGL := true
-
-# Charging mode
-BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
-BOARD_BATTERY_DEVICE_NAME := "battery"
-BOARD_CHARGER_RES := device/samsung/galaxys2-common/res/charger
-
-BOARD_SUPPRESS_EMMC_WIPE := true
 
 # Notification LED
 BOARD_HAS_LED_NOTIF := true
 
 # HWComposer
-BOARD_USES_HWCOMPOSER := true
-BOARD_USE_SECTVOUT := true
-BOARD_USES_FIMGAPI := true
 BOARD_SAMSUNG_TVOUT := true
 BOARD_HDMI_DDC_CH := DDC_CH_I2C_7
-BOARD_USES_PROPRIETARY_LIBFIMC := true
-#BOARD_HAVE_HDMI_SUPPORT := SAMSUNG_HDMI_SUPPORT
-#BOARD_USES_SAMSUNG_HDMI := true
-
-# OMX
-BOARD_HAVE_CODEC_SUPPORT := SAMSUNG_CODEC_SUPPORT
-COMMON_GLOBAL_CFLAGS += -DSAMSUNG_CODEC_SUPPORT
-BOARD_NONBLOCK_MODE_PROCESS := true
-BOARD_USE_STOREMETADATA := true
-BOARD_USE_METADATABUFFERTYPE := true
-BOARD_USES_MFC_FPS := true
-
-# Audio
-BOARD_USE_YAMAHAPLAYER := true
-BOARD_USE_SAMSUNG_SEPARATEDSTREAM := true
-BOARD_HAS_SAMSUNG_VOLUME_BUG := true
-COMMON_GLOBAL_CFLAGS += -DICS_AUDIO_BLOB
-
-# Camera
-COMMON_GLOBAL_CFLAGS += -DSAMSUNG_CAMERA_HARDWARE
-BOARD_USES_PROPRIETARY_LIBCAMERA := true
-COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
-DISABLE_HW_ID_MATCH_CHECK := true
 
 # RIL
 BOARD_MOBILEDATA_INTERFACE_NAME := "ppp0"
 
-# Wifi
-BOARD_WLAN_DEVICE                := bcmdhd
-BOARD_WLAN_DEVICE_REV            := bcm4330_b1
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/dhd.ko"
-WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcm4330_sta.bin"
-WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcm4330_apsta.bin"
-WIFI_DRIVER_FW_PATH_P2P          := "/system/etc/wifi/bcm4330_p2p.bin"
-WIFI_DRIVER_MODULE_NAME          := "dhd"
-WIFI_DRIVER_MODULE_ARG           := "firmware_path=/system/etc/wifi/bcm4330_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
-WIFI_BAND                        := 802_11_ABG
-BOARD_LEGACY_NL80211_STA_EVENTS  := true
-
-# Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_BCM := true
-
-# Vold
-BOARD_VOLD_MAX_PARTITIONS := 12
-BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
-BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/s3c-usbgadget/gadget/lun%d/file"
-
 # Recovery
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/d710/recovery/recovery_keys.c
-BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/galaxys2-common/recovery/graphics.c
 BOARD_UMS_LUNFILE := "/sys/devices/platform/s3c-usbgadget/gadget/lun%d/file"
 TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_USES_MMCUTILS := true
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_HAS_NO_SELECT_BUTTON := true
 
 TARGET_SPECIFIC_HEADER_PATH := device/samsung/d710/include
-
-# Nearly all shipped I9100 devices have defective eMMC chips (VYL00M fwrev 0x19)
-# Prevent usage of ERASE commands in recovery on these boards.
-# This is redundant for our recovery since the kernel has MMC_CAP_ERASE
-# disabled for mshci.c, and so do nearly all I9100 kernels,
-# but better safe than sorry.
-BOARD_SUPPRESS_EMMC_WIPE := true
-
+EXYNOS4210_ENHANCEMENTS := true
 # assert
 TARGET_OTA_ASSERT_DEVICE := epic4gtouch,SPH-D710,d710
 
