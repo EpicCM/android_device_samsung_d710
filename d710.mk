718ed070071c33440859701fdf2b2e7d677292db
chris41g <chriswhite41g@gmail.com>
2012-10-14 23:11:46 -0500

    changes to allow r760 to pull

--- d710.mk
+++ d710.mk
@@ -108,13 +108,15 @@ PRODUCT_PACKAGES += \
     static_busybox \
     make_ext4fs \
     setup_fs
-	
+
+ifeq ($(TARGET_DEVICE),d710)	
 # WIMAX
 PRODUCT_PACKAGES += \
     WiMAXSettings \
     SprintMenu \
     WiMAXHiddenMenu \
     SystemUpdateUI 
+endif
 	
 # MFC API
 PRODUCT_PACKAGES += \
